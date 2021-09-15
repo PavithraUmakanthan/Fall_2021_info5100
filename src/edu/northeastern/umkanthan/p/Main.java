6411f5e80a4a654bdc756fb78753afc4392630c0
Pavi <u.pavithraa@gmail.com>
2021-09-14 21:25:37 -0700

    primitive type

---
+++ src/edu/northeastern/umkanthan/p/Main.java
@@ -0,0 +1,44 @@
+package edu.northeastern.umkanthan.p;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        int numOfStudentsInClass = 62;
+
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+
+        short maxShort = Short.MAX_VALUE;
+        short minShort = Short.MIN_VALUE;
+
+        double maxDouble = Double.MAX_VALUE;
+        double minDouble = Double.MIN_VALUE;
+        Double myval = 5.23d;
+
+        char ch = 'A';
+
+        System.out.println("My" + ch);
+
+        boolean bTestVal = true;
+        System.out.println(bTestVal);
+        bTestVal =!bTestVal;
+        System.out.println(bTestVal);
+
+        long maxlong = Long.MAX_VALUE;
+        long minlong = Long.MIN_VALUE;
+        long myvalue = 5;
+
+        byte maxbyte = Byte.MAX_VALUE;
+        byte minbyte = Byte.MIN_VALUE;
+
+
+
+        System.out.println(maxbyte);
+        System.out.println(minbyte);
+        System.out.println(myvalue);
+
+    }
+}
